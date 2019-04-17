4ab172e7f4d844ccd3c7a05e76bac07ae2fc8347
kangswx <996334837@qq.com>
2019-04-17 09:23:01 +0800

    修改findById

--- product-service/src/main/java/com/swkang/springcloud/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/swkang/springcloud/productservice/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.swkang.springcloud.productservice.controller;
 
+import com.swkang.springcloud.productservice.domain.Product;
 import com.swkang.springcloud.productservice.service.ProductService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/product")
 public class ProductController {
+
+    @Value("${server.port}")
+    private String port;
 
     @Autowired
     private ProductService productService;
@@ -29,6 +35,10 @@ public class ProductController {
      */
     @RequestMapping("find")
     public Object findById(@RequestParam("id") Integer id){
-        return productService.findById(id);
+        Product product = productService.findById(id);
+        Product result = new Product();
+        BeanUtils.copyProperties(product, result);
+        result.setName(result.getName() + " data from port = " +port);
+        return result;
     }
 }

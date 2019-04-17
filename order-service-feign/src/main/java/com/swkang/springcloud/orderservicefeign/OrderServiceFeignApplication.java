94e66b77398889e00f29b81eb90462714a072374
kangswx <996334837@qq.com>
2019-04-17 17:10:29 +0800

    增加EnableCircuitBreaker注释

--- order-service-feign/src/main/java/com/swkang/springcloud/orderservicefeign/OrderServiceFeignApplication.java
+++ order-service-feign/src/main/java/com/swkang/springcloud/orderservicefeign/OrderServiceFeignApplication.java
@@ -2,10 +2,12 @@ package com.swkang.springcloud.orderservicefeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients    //对Feign的支持
+@EnableCircuitBreaker  //对hystrix的支持
 public class OrderServiceFeignApplication {
 
     public static void main(String[] args) {

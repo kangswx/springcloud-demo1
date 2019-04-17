d777ab59c8b47f2372838cf251f30b9228691b03
kangswx <996334837@qq.com>
2019-04-17 20:56:05 +0800

    增加EnableHystrixDashboard

--- order-service-feign/src/main/java/com/swkang/springcloud/orderservicefeign/OrderServiceFeignApplication.java
+++ order-service-feign/src/main/java/com/swkang/springcloud/orderservicefeign/OrderServiceFeignApplication.java
@@ -3,11 +3,13 @@ package com.swkang.springcloud.orderservicefeign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients    //对Feign的支持
-@EnableCircuitBreaker  //对hystrix的支持
+@EnableFeignClients     //对Feign的支持
+@EnableCircuitBreaker   //对hystrix的支持
+@EnableHystrixDashboard //对仪表盘监控的支持
 public class OrderServiceFeignApplication {
 
     public static void main(String[] args) {

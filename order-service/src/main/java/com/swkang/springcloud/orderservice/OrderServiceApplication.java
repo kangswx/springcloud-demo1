0ec7a9fd0cafd17ee218c4492c35259cd696586d
kangswx <996334837@qq.com>
2019-04-17 10:06:41 +0800

    新增OrderServiceApplication

--- order-service/src/main/java/com/swkang/springcloud/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/swkang/springcloud/orderservice/OrderServiceApplication.java
@@ -2,12 +2,21 @@ package com.swkang.springcloud.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced       //标记为负载均衡器
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }

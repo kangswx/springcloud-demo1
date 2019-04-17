b2b3e5abf387544c61c66ce993ab063da16d5218
kangswx <996334837@qq.com>
2019-04-17 14:52:13 +0800

    代码优化

--- order-service/src/main/java/com/swkang/springcloud/orderservice/service/impl/ProductOrderServiceImpl.java
+++ order-service/src/main/java/com/swkang/springcloud/orderservice/service/impl/ProductOrderServiceImpl.java
@@ -3,10 +3,12 @@ package com.swkang.springcloud.orderservice.service.impl;
 import com.swkang.springcloud.orderservice.domain.ProductOrder;
 import com.swkang.springcloud.orderservice.service.ProductOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -14,17 +16,20 @@ public class ProductOrderServiceImpl implements ProductOrderService {
 
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private LoadBalancerClient loadBalancer;
 
     @Override
     public ProductOrder save(int userId, int productId) {
 
-        Object o = restTemplate.getForObject("http://product-service/api/v1/product/find?id="+productId, Object.class);
-        System.out.println("o"+o.toString());
+        Map<String, Object> pro = restTemplate.getForObject("http://product-service/api/v1/product/find?id="+productId, Map.class);
+
         ProductOrder productOrder = new ProductOrder();
         productOrder.setCreateTime(new Date());
         productOrder.setUserId(userId);
         productOrder.setTradeNo(UUID.randomUUID().toString());
-
+        productOrder.setProductName(pro.get("name").toString());
+        productOrder.setPrice(Double.parseDouble(pro.get("price").toString()));
 
         return productOrder;
     }

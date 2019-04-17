942340d63139e468ff9ffb2425900ccd60a42d4c
kangswx <996334837@qq.com>
2019-04-17 17:30:59 +0800

    增加fallback

--- order-service-feign/src/main/java/com/swkang/springcloud/orderservicefeign/service/ProductClient.java
+++ order-service-feign/src/main/java/com/swkang/springcloud/orderservicefeign/service/ProductClient.java
@@ -1,5 +1,6 @@
 package com.swkang.springcloud.orderservicefeign.service;
 
+import com.swkang.springcloud.orderservicefeign.fallback.ProductClientFallBack;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * 商品服务客户端
  */
-@FeignClient(name = "product-service")
+@FeignClient(name = "product-service", fallback = ProductClientFallBack.class)
 public interface ProductClient {
 
     @GetMapping("/api/v1/product/find")  //路径不能错

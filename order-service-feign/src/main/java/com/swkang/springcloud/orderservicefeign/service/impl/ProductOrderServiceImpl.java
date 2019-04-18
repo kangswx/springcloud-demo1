db84df9f8b739bcc5b62a99a4519ac4f9bb81a59
kangswx <996334837@qq.com>
2019-04-18 21:35:09 +0800

    增加logger

--- order-service-feign/src/main/java/com/swkang/springcloud/orderservicefeign/service/impl/ProductOrderServiceImpl.java
+++ order-service-feign/src/main/java/com/swkang/springcloud/orderservicefeign/service/impl/ProductOrderServiceImpl.java
@@ -5,6 +5,8 @@ import com.swkang.springcloud.orderservicefeign.domain.ProductOrder;
 import com.swkang.springcloud.orderservicefeign.service.ProductClient;
 import com.swkang.springcloud.orderservicefeign.service.ProductOrderService;
 import com.swkang.springcloud.orderservicefeign.utils.JsonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +16,14 @@ import java.util.UUID;
 @Service
 public class ProductOrderServiceImpl implements ProductOrderService {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private ProductClient productClient;
 
     @Override
     public ProductOrder save(int userId, int productId) {
-
+        logger.info("service save order");
         if(1 == userId){
             return null;
         }
